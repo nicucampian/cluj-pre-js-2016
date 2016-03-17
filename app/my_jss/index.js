de9f984818979f8815cfce0e2cf6000842a83c64
Septimiu Criste <septimiu.criste@softvision.ro>
2016-03-17 13:50:16 +0200

    commit day 4 trainning
    Tema ziua 4
    
    login page modifications on css
    
    adding css styles and javascript functions on pages

---
+++ app/my_jss/index.js
@@ -0,0 +1,13 @@
+var playlist;
+
+playlist = document.getElementById('playlistAbsolut');
+
+function showPlaylist(){
+
+playlist.style.display = 'inline';
+}
+
+function hidePlaylist(){
+
+playlist.style.display = 'none';
+}
